325edf8b075e3d6e5939daf27ae5ded102e91b42
HumbleFrog-LeetCode <pratulyadav21@gmail.com>
2024-08-08 22:49:26 +0530

    Time: 12 ms (59.37%), Space: 14.4 MB (24.50%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for(int i = 0; i < nums.size(); i++) {
+            if(mp.find(target - nums[i]) != mp.end())
+                return {mp[target - nums[i]], i};
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
