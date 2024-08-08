890a32acdad16ba579dc5a61534f76e2e8923af8
HumbleFrog-LeetCode <pratulyadav21@gmail.com>
2024-08-08 23:05:47 +0530

    Time: 22 ms (95.94%), Space: 21.7 MB (27.68%) - LeetHub

---
+++ 0757-set-intersection-size-at-least-two/0757-set-intersection-size-at-least-two.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    static bool compare(vector<int> &a, vector<int> &b) {
+        if(a[1] == b[1])
+            return a[0] < b[0];
+        return a[1] < b[1];    
+    }
+
+    int intersectionSizeTwo(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), compare);
+        vector<int> res;
+        res.push_back(intervals[0][1] - 1);
+        res.push_back(intervals[0][1]);
+        for(int i = 1; i < intervals.size(); i++) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            if(start > res.back()) {
+                res.push_back(end - 1);
+                res.push_back(end);
+            }
+            else if(start == res.back() || start > res[res.size() - 2])
+                res.push_back(end);
+        }
+        return res.size();
+    }
+};
